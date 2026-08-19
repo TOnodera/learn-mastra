233f69ec6343a99efa4ca59466e1151a19071495
onodera <onodera@h-company.co.jp>
2026-08-19 08:40:23 +0000

    セマンティックリコールの実装まで

--- image-ai-service/src/mastra/create-memory.ts
+++ image-ai-service/src/mastra/create-memory.ts
@@ -1,6 +1,15 @@
+import { ModelRouterEmbeddingModel } from "@mastra/core/llm";
+import { LibSQLVector } from "@mastra/libsql";
 import { Memory } from "@mastra/memory";
 
 export const memory = new Memory({
+  // 埋め込みモデル
+  embedder: new ModelRouterEmbeddingModel("openai/text-embedding-3-small"),
+  // ベクトルDB
+  vector: new LibSQLVector({
+    id: "image-support-agent-vector",
+    url: "file:./local.db"
+  }),
   options: {
     // 直近１０のメッセージが自動的に取り込まれる
     lastMessages: 10,
@@ -23,6 +32,11 @@ export const memory = new Memory({
 ## 直近のコンテキスト
 - 最後の生成リクエスト: [なし]
 - 進行中のプロジェクト: [なし]`
+    },
+    semanticRecall: {
+      topK: 3, // 取得する類似メッセージの件数
+      messageRange: 2, // 取得したメッセージの前後に含める文脈の件数
+      scope: "resource" // thread or resource resourceはユーザーの長期記憶を活かせる。threadは対象のスレッドのみ
     }
   }
 });
